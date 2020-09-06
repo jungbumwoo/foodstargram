fc85d2ecca7522d032d125578fede66eb12e55d8
jungbum <jungbumwoo@naver.com>
2020-09-06 18:28:29 +0900

    0906

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const port = 3000;
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(morgan("dev"));
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.send('Hello World');    
 });
 
 const handleListen = () => {
-    console.log(`✅ Listening on ${port} port`)
-}
+    console.log(`✅ Listening on ${port} port`);
+};
 
 app.listen(port, handleListen);
